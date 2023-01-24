0ef1b584b8ef11aefb301cfbc633b53b0eedc464
Ilgar <IlgarAbdullazade>
2023-01-24 12:37:32 +0400

    Updates

--- assets/Paragraph-58e6a78a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as r,c as t,r as s,a as o,t as _}from"./index-2e795f4a.js";const n={name:"HeraParagraph"},p={class:"paragraph"};function c(a,d,l,i,u,f){return r(),t("p",p,[s(a.$slots,"default",{},()=>[o(_(a.text),1)],!0)])}const h=e(n,[["render",c],["__scopeId","data-v-547640d0"]]);export{h as H};
